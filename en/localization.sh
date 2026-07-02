9283d6550cfa043700b9e042c9a37b194944720d
Mikhail Shchekotov <torunar@gmail.com>
2026-07-02 14:34:35 +0200

    change title

--- en/localization.sh
+++ en/localization.sh
@@ -3,6 +3,6 @@ CIDER_mainTitlePaged="${CIDER_mainTitle} :: Page <!--{var:pageNumber}-->"
 CIDER_pageLinkPrevText="&larr;"
 CIDER_pageLinkNextText="&rarr;"
 CIDER_pageLinkCurrText="<!--{var:pageNumber}-->"
-CIDER_blogName="# /usr/bin/torunar --locale=en-US"
+CIDER_blogName="# /usr/bin/torunar --lang=en"
 CIDER_blogDescription=""
 CIDER_footerLinks='<a class="rss" href="/en/rss.xml">RSS</a> <a class="github" href="https://github.com/torunar/">GitHub</a>'
